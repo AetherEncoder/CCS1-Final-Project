250c5f76fbaed392bb23038b4c3d421780accc0a
AetherEncoder <saikopewds@gmail.com>
2024-11-29 21:20:50 +0800

    Init

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <html>
   <head>
     <title>CCS 1 - Final Project</title>
+
+    <link rel="stylesheet" type="text/css" href="src/styles.css">
+    <link rel="icon" type="x/icon" href="src/images/bluelock_log.png">
+
   </head>
 
   <body>
-    <h1>HELLO WORLD</h1>
+    <h1>Hello World</h1>
+
+    
+
+    <script src="src/script.js"></script>
   </body>
 </html>
